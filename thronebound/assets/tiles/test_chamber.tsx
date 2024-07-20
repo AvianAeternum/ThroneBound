87a025c9db5c0a996dc3468ca62839f9cb1aeb8c
Aidan Aeternum <aidanaeternum@gmail.com>
2024-07-19 23:45:51 -0700

    Some Updates

---
+++ thronebound/assets/tiles/test_chamber.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Testing" tilewidth="32" tileheight="32" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="tiles/Sprite-0002.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="tiles/Sprite-0003.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="tiles/Sprite-0004.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="tiles/Sprite-0005.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="tiles/Sprite-0001.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="tiles/Sprite-0006.png" width="32" height="32"/>
+ </tile>
+</tileset>
