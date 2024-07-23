4a3dc0abc9428e54b8d657f3a1984b1a6d2b414b
Aidan Aeternum <aidanaeternum@gmail.com>
2024-07-22 23:43:12 -0700

    Updated map

--- thronebound/assets/world/tileset/test.tsx
+++ thronebound/assets/world/tileset/test.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="test" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="test.png" width="320" height="320"/>
+<tileset version="1.10" tiledversion="1.11.0" name="test" tilewidth="32" tileheight="32" tilecount="10000" columns="100">
+ <image source="test.png" width="3200" height="3200"/>
 </tileset>
